59bf81b1fa1a31afcb5b8dd62e6004570b9275aa
konstantin.belik <belikcost@yandex.ru>
2023-01-21 17:34:19 +0300

    Добавлена заглушка при пустом списке

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -8,55 +8,60 @@
           v-if="headerText"
           v-text="headerText"
       />
-      <template v-for="(item, index) in users">
-        <v-divider
-            v-if="index !== 0 && index !== users.length - 1"
-            :key="index"
-            :inset="true"
-        />
-        <v-list-item :key="item.title">
-          <v-list-item-avatar>
+      <template v-if="users.length">
+        <template v-for="(item, index) in users">
+          <v-divider
+              v-if="index !== 0 && index !== users.length - 1"
+              :key="index"
+              :inset="true"
+          />
+          <v-list-item :key="item.title">
+            <v-list-item-avatar>
 
-            <v-menu
-                bottom
-                min-width="200px"
-                rounded
-                offset-y
-            >
-              <template v-slot:activator="{ on }">
-                <v-btn
-                    icon
-                    x-large
-                    v-on="on"
-                >
-                  <v-avatar
-                      size="48"
+              <v-menu
+                  bottom
+                  min-width="200px"
+                  rounded
+                  offset-y
+              >
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                      icon
+                      x-large
+                      v-on="on"
                   >
-                    <v-img :src="item.avatar"/>
-                  </v-avatar>
-                </v-btn>
-              </template>
-              <v-card>
-                <p class="text-caption ma-0 px-1">
-                  {{ item.address }}
-                </p>
-              </v-card>
-            </v-menu>
-          </v-list-item-avatar>
+                    <v-avatar
+                        size="48"
+                    >
+                      <v-img :src="item.avatar"/>
+                    </v-avatar>
+                  </v-btn>
+                </template>
+                <v-card>
+                  <p class="text-caption ma-0 px-1">
+                    {{ item.address }}
+                  </p>
+                </v-card>
+              </v-menu>
+            </v-list-item-avatar>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }} <span class="grey--text text--lighten-1" v-if="item.score"
-                                                      title="Score">{{ item.score }}</span>
-            </v-list-item-title>
-            <div>
-              <v-chip small>
-                {{ item.country }}
-              </v-chip>
-            </div>
-            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }} <span class="grey--text text--lighten-1" v-if="item.score"
+                                                        title="Score">{{ item.score }}</span>
+              </v-list-item-title>
+              <div>
+                <v-chip small>
+                  {{ item.country }}
+                </v-chip>
+              </div>
+              <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
       </template>
+      <p v-else class="text-caption px-2 ma-0">
+        Нет результатов
+      </p>
     </v-list>
   </v-card>
 </template>
